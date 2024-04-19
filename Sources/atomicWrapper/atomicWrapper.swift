463650295c726efc3c2a17f35af19d43e44c4fa6
Carl Hung <carl.hung@yahoo.com>
2024-04-19 20:19:36 +0000

    first commit

---
+++ Sources/atomicWrapper/atomicWrapper.swift
@@ -0,0 +1,58 @@
+import Foundation
+
+@propertyWrapper
+public final class Atomic<T> {
+    
+    @usableFromInline
+    let locker: NSLock = NSLock()
+
+    @usableFromInline
+    var storedValue: T
+
+    @inlinable
+    public init(wrappedValue: T) {
+        self.storedValue = wrappedValue
+    }
+
+    @inlinable
+    public init(_ wrappedValue: T) {
+        self.storedValue = wrappedValue
+    }
+
+    @inlinable
+    public var wrappedValue: T {
+        _read {
+            locker.lock()
+            defer {
+                locker.unlock()
+            }
+            yield storedValue
+        }
+        _modify {
+            locker.lock()
+            defer {
+                locker.unlock()
+            }
+            yield &storedValue
+        }
+        set {
+            locker.lock()
+            defer {
+                locker.unlock()
+            }
+            storedValue = newValue
+        }
+    }
+
+    @discardableResult
+    @inlinable
+    public func safe<R>(_ execute: (inout T) throws -> R) rethrows -> R {
+        locker.lock()
+        defer {
+            locker.unlock()
+        }
+        return try execute(&storedValue)
+    }
+}
+
+extension Atomic: @unchecked Sendable where T: Sendable {}
---
+++ Tests/atomicWrapperTests/atomicWrapperTests.swift
@@ -0,0 +1,40 @@
+import XCTest
+@testable import atomicWrapper
+
+final class atomicWrapperTests: XCTestCase, @unchecked Sendable {
+
+    let iterations = 100_000_000
+
+    @Atomic
+    var arr = [Int]()
+
+    func testAsPropertyWrapper() async {
+        await performAsync(iterations: iterations) {
+            self.arr.append(0)
+        }
+
+        XCTAssertEqual(arr.count, iterations)
+    }
+
+    func testAsWrapper() async {
+        let arr = Atomic([Int]())
+        await performAsync(iterations: iterations) {
+            arr.safe {
+                $0.append(0)
+            }
+        }
+
+        XCTAssertEqual(arr.safe(\.count), iterations)
+    }
+
+    private func performAsync(iterations: Int, execute: @Sendable @escaping () -> Void) async {
+        await withCheckedContinuation { continuation in
+            DispatchQueue.global().async {   
+                DispatchQueue.concurrentPerform(iterations: iterations) { _ in
+                    execute()
+                }
+                continuation.resume(returning: ())
+            }
+        }
+    }
+}
