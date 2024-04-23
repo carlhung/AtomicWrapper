458183a016a4e6e833e0049c2b4585ef211a972a
Carl Hung <carl.hung@yahoo.com>
2024-04-23 15:45:05 +0000

    added test

--- Tests/atomicWrapperTests/atomicWrapperTests.swift
+++ Tests/atomicWrapperTests/atomicWrapperTests.swift
@@ -1,9 +1,21 @@
 import XCTest
 @testable import AtomicWrapper
 
+
+struct SomeThing {
+    @Atomic 
+    var arr = [Int]()
+
+    func run() {
+        arr.append(0)
+    }
+}
+
 final class AtomicWrapperTests: XCTestCase, @unchecked Sendable {
 
     let iterations = 100_000_000
+
+    var something = SomeThing()
 
     @Atomic
     var arr = [Int]()
@@ -27,14 +39,21 @@ final class AtomicWrapperTests: XCTestCase, @unchecked Sendable {
         XCTAssertEqual(arr.safe(\.count), iterations)
     }
 
-    private func performAsync(iterations: Int, execute: @Sendable @escaping () -> Void) async {
-        await withCheckedContinuation { continuation in
-            DispatchQueue.global().async {   
-                DispatchQueue.concurrentPerform(iterations: iterations) { _ in
-                    execute()
-                }
-                continuation.resume(returning: ())
+    func testStruct() async {
+        await performAsync(iterations: iterations) {
+            self.something.run()
+        }
+        XCTAssertEqual(something.arr.count, iterations)
+    }
+}
+
+private func performAsync(iterations: Int, execute: @Sendable @escaping () -> Void) async {
+    await withCheckedContinuation { continuation in
+        DispatchQueue.global().async {   
+            DispatchQueue.concurrentPerform(iterations: iterations) { _ in
+                execute()
             }
+            continuation.resume(returning: ())
         }
     }
 }
